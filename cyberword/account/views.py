4d24516194da9adc4fae524aa18ba8f6afcdff68
Oukey <oukey311@gmail.com>
2020-04-20 22:07:37 +0400

    Добавлена регистрация пользователя

--- cyberword/account/views.py
+++ cyberword/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -11,6 +11,7 @@ def dashboard(request):
 
 
 def user_login(request):
+    # Вход в систему
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -29,3 +30,22 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    # Регистрация пользователя
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создание нового пользователя без сохранения в БД
+            new_user = user_form.save(commit=False)
+            # Задание пользователю зашифрованного пароля
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохранение пользователя в БД
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
